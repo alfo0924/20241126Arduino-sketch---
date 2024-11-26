a862a7baff97c6ba37a1cb90a37f839005aa0736
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-11-26 15:26:39 +0800

    Update sketch_nov26c.ino

--- sketch_nov26c.ino
+++ sketch_nov26c.ino
@@ -1,1 +1,23 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID ""
+#define BLYNK_TEMPLATE_NAME ""
+#define BLYNK_AUTH_TOKEN ""
 
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// Your WiFi credentials
+char ssid[] = " wifiName ";
+char pass[] = " wifiCode ";
+
+void setup()
+{
+    Serial.begin(115200);
+    Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
+
+void loop()
+{
+    Blynk.run();
+}
