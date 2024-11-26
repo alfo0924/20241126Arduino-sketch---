45938d8b7f46de3d1b2b356c930057318d2f97cd
alfo0924 <146694784+alfo0924@users.noreply.github.com>
2024-11-26 13:34:36 +0800

    Update sketch_nov26a.ino

--- sketch_nov26a.ino
+++ sketch_nov26a.ino
@@ -1,13 +1,12 @@
-
 void setup() {
-  // put your setup code here, to run once:
-pinMode(13,INPUT);
-Serial.begin(9600);
+  // 設定初始化，此段程式只會執行一次
+  pinMode(13, INPUT);    // 設定腳位13為輸入模式
+  Serial.begin(9600);    // 啟動序列通訊，鮑率設為9600
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-int light_value = analogRead(13);
-Serial.println(light_value);
-delay(100);
+  // 主要程式區塊，會重複執行
+  int light_value = analogRead(13);    // 讀取腳位13的類比值並儲存到 light_value 變數
+  Serial.println(light_value);         // 將讀取到的數值透過序列埠輸出
+  delay(100);                          // 延遲100毫秒再執行下一次讀取
 }
